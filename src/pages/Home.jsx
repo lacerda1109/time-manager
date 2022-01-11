a8725583dd9174196afcacd35e47fac9875e0124
Gabriel Lacerda <devlacerda1109@gmail.com>
2022-01-11 17:51:07 -0300

    Introduz interface da página home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,31 @@
+import Page from '../components/Page'
 import Button from '../components/Button'
+import { Link } from 'react-router-dom'
+import './Home.css'
 
 export default function Home() {
     return (
         <div>
+            <Page>
+                <div
+                    className="mainContainer"
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        textAlign: 'center',
+                        height: '800px',
+                    }}
+                >
+                    <h1>Time Manager</h1>
+                    <p style={{margin: '50px 0'}}>Uma simples, rápida e minimalista aplicação<br />web para gerenciar seu tempo. Atinja<br />um dia mais produtivo.</p>
+                    <div style={{display: 'flex', gap: '15px'}}>
+                        <Button theme="default" text="Começar" />
+                        <Link to="/sobre"><Button theme="secondary" text="Saiba mais" /></Link>
+                    </div>
+                </div>
+            </Page>
         </div>
     )
 }
