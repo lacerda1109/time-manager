6219811979687e8780cda542067cacc57cc6a767
Gabriel Lacerda <devlacerda1109@gmail.com>
2022-01-19 11:07:35 -0300

    Adiciona renderização do quadro de voltas (click botão volta)

--- src/pages/Chronometer.jsx
+++ src/pages/Chronometer.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { palette } from '../theme/palette'
 import Page from '../components/Page'
 import ChronClock from '../components/ChronClock'
 import Button from '../components/Button'
@@ -12,6 +13,46 @@ export default function Chronometer() {
         setStarted(true)
         setPaused(false)
     }
+
+    // MUDANÇAS NA INTERFACE --------------------------------------------------------------------------------
+    const [stepBox, setStepBox] = useState(false)
+
+    function step() {
+        setStepBox(true)
+    }
+
+    let [arrStep, setArrStep] = useState([
+        '00:02:424',
+        '00:04:837',
+        // '00:06:637',
+    ])
+    console.log(arrStep)
+
+    let stepBoxLayout = (<div
+        style={{
+            width: '230px',
+            overflow: 'auto',
+            height: '135px',
+            padding: '20px',
+            backgroundColor: palette.secondaryColor,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '15px',
+        }}
+    >
+        <div style={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '7px' }}>
+            <p>mock</p>
+            {arrStep.map((el, i) => {
+                return (
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <p>#{i + 1}</p>
+                        <p>{el}</p>
+                    </div>
+                )
+            })}
+        </div>
+    </div>)
 
     return (
         <Page>
@@ -30,7 +71,7 @@ export default function Chronometer() {
                         <div>
                             <Button theme="red" text="Recomeçar" />
                         </div>
-                        <div>
+                        <div onClick={() => step()}>
                             <Button theme="orange" text="Volta" />
                         </div>
                         <div onClick={() => setPaused(!paused)}>
@@ -38,6 +79,7 @@ export default function Chronometer() {
                         </div>
                     </div>
                 )}
+                {stepBox ? (stepBoxLayout) : null}
             </div>
         </Page>
     )
