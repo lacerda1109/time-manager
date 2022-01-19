7b00d515ea6315da4614995bdcdeed661424b2f8
Gabriel Lacerda <devlacerda1109@gmail.com>
2022-01-19 10:02:46 -0300

    Adiciona fluxo dos botões da página Cronômetro

--- src/pages/Chronometer.jsx
+++ src/pages/Chronometer.jsx
@@ -1,8 +1,17 @@
+import { useState } from 'react'
 import Page from '../components/Page'
 import ChronClock from '../components/ChronClock'
 import Button from '../components/Button'
 
 export default function Chronometer() {
+    const [started, setStarted] = useState(false)
+    const [paused, setPaused] = useState(true)
+
+    // FUNÇÕES DO CRONÔMETRO --------------------------------------------------------------------------------
+    function startChronometer() {
+        setStarted(true)
+        setPaused(false)
+    }
 
     return (
         <Page>
@@ -12,9 +21,23 @@ export default function Chronometer() {
                     seconds="00"
                     milisec="00"
                 />
-                <div>
-                    <Button theme="default" text="Começar" />
-                </div>
+                {!started ? (
+                    <div onClick={() => startChronometer()}>
+                        <Button theme="default" text="Começar" />
+                    </div>
+                ) : (
+                    <div style={{ display: 'flex', gap: '15px' }}>
+                        <div>
+                            <Button theme="red" text="Recomeçar" />
+                        </div>
+                        <div>
+                            <Button theme="orange" text="Volta" />
+                        </div>
+                        <div onClick={() => setPaused(!paused)}>
+                            <Button theme="default" text={paused ? "Continuar" : "Pausar"} />
+                        </div>
+                    </div>
+                )}
             </div>
         </Page>
     )
